91eb264ae53ec79569f1d337f3542c8f868a7c4c
myfCode <mayuefeng90@126.com>
2018-11-24 16:24:45 +0800

    redux

--- src/applyMiddleware.js
+++ src/applyMiddleware.js
@@ -1,5 +1,15 @@
 import compose from './compose'
 
+/***
+ *  applyMiddleware适用于处理store中间件的
+ *
+ *  中间件的定义方式：
+ *  ( {getState, dispatch} ) => next => action => { 你写中间件的操作 ；如果需要继续往下执行，使用next（action）；如果不需要，就不要next了 }
+ *  getState,dispatch 是store里的getState和dispatch
+ *
+ *
+ */
+
 // 方法一
 let applyMiddleware1 = (...middlewares) => {
     return createStore => (...args) => {
@@ -29,13 +39,22 @@ let applyMiddleware1 = (...middlewares) => {
 
 
 
-//方法二
+//方法二，是中间件的更易读的形式
 let applyMiddleware2 = function (...middlewares) {
     return function (storeFn) {
         return function (...args) {
 
+            /***
+             * 参数详解
+             * 1、middlewares： 由各种中间件组成的list
+             * 2、storeFn：实际上就是createStore
+             * 3、args：就是createStore需要的reducer和preloadState
+             */
+
+            //生成一个store容器，...args就是reducer和preloadState
             let store = storeFn(...args)
 
+            //重新定义一个dispatch，最终目的是用于覆盖上面生成的store默认dispatch
             let dispatch = (...params) => {
                 throw new Error('disptach has not init')
             }
@@ -46,7 +65,20 @@ let applyMiddleware2 = function (...middlewares) {
                 dispatch: (...dispatchParams) => dispatch(...dispatchParams)
             }
 
+            /***
+             * 初始化一下中间件
+             * 使每个中间件
+             * ( {dispatch, getState} ) => next => action => {一些操作； next(action)}
+             * 转化为
+             * next => action => {一些操作； next(action)}
+             *
+             * 由于map方法，所以生成的chain，是转化后的list
+             * ***/
             let chain = middlewares.map(middleware => middleware(initStoreApi))
+
+            /***
+             *
+             */
             dispatch = compose(...chain)(store.dispatch)
 
             return {
--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,30 @@
+/***
+ * createStore用于生成一个容器，容器里存储了页面的状态；同时返回了用于单项修改、获取状态的方法
+ * @param reducer
+ * @param preloadState
+ * @param enhancer
+ * @returns {*}
+ */
 function createStore(reducer, preloadState, enhancer) {
 
+    /***
+     *根据参数传的不同，进行不同的操作
+     *  1、默认createStore有三个参数reducer、preloadState、enhancer，如果参数正常执行enhancer(createStore)(reducer, preloadState)
+     *  2、如果只有两个参数
+     *     如果 typeof reducer != 'function' 不是函数，就报错
+     *     如果 typeof preloadState === '函数' && echancer == undefined （也就是说只穿了连个参数）；那就把preloadState赋给enhancer， 然后自己置为undefined，
+     *     如果 preloadState === {} 对象，就复制为默认的_state
+     *     如果 preloadState == undefined ，默认_state = {}
+     *
+     * ***/
+
+
+    // 如果 typeof reducer != 'function' 不是函数，就报错
+    if(typeof reducer != 'function'){
+        throw new Error('reducer must be function')
+    }
+
+    // 如果 typeof preloadState === '函数' && echancer == undefined （也就是说只穿了连个参数）；那就把preloadState赋给enhancer， 然后自己置为undefined，
     if(typeof preloadState === 'function' && enhancer == undefined){
         enhancer = preloadState
         preloadState = undefined
@@ -10,22 +35,32 @@ function createStore(reducer, preloadState, enhancer) {
     }
 
 
-    if(typeof reducer != 'function' && typeof preloadState === 'function'){
-        reducer = preloadState
-        preloadState = undefined
-    }
 
-    if(typeof  reducer !== 'function'){
-        throw new Error('reducer must is function')
-    }
-
+    /***
+     *1、定义状态对象_state, 外部访问需要使用getState方法
+     *2、没有传默认的preloadState，就设置_state = {}
+     * ***/
     let _state = preloadState || {}
+
+    /***
+     * 默认监听事件存放的list
+     * ***/
     let listeners = []
 
+
+    /***
+     * 获取内部的_state
+     * @returns {*|{}}
+     */
     function getState() {
         return _state
     }
 
+    /***
+     * 分发action；很关键的一点是，最后返回action
+     * @param action
+     * @returns {*}
+     */
     function dispatch(action) {
         _state = reducer(_state, action)
 
@@ -36,6 +71,11 @@ function createStore(reducer, preloadState, enhancer) {
         return action
     }
 
+    /***
+     * 订阅事件
+     * @param fn
+     * @returns {Function}
+     */
     function subscribe(fn) {
         typeof fn === 'function' && listeners.push(fn)
 
@@ -44,12 +84,19 @@ function createStore(reducer, preloadState, enhancer) {
         }
     }
 
+    /***
+     * 替换reducer
+     * @param nextReducer
+     */
     function replaceReducer(nextReducer) {
         if(nextReducer != undefined && typeof nextReducer === 'function') {
             reducer = nextReducer
         }
     }
 
+    /***
+     * 返回store的方法
+     */
     return {
         getState,
         dispatch,
